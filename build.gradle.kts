eaa4c0bc9856fdf29f067979c82309db91545a9b
pheonic <thepheonic@gmail.com>
2018-07-28 23:59:18 -0700

    Add fatJar task
    from https://stackoverflow.com/questions/41794914/how-to-create-the-fat-jar-with-gradle-kotlin-script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.gradle.jvm.tasks.Jar
 
 plugins {
     java
@@ -15,24 +16,36 @@ repositories {
 
 dependencies {
     compile(kotlin("stdlib-jdk8"))
-    compile(group = "com.discord4j",
-            name = "Discord4J",
-            version = "2.10.1")
-    compile(group = "com.sedmelluq",
-            name = "lavaplayer",
-            version = "1.3.7")
-    compile(group = "ch.qos.logback",
-            name = "logback-classic",
-            version = "1.2.3")
-    compile(group = "io.github.microutils",
-            name = "kotlin-logging",
-            version = "1.5.4")
-    compile(group = "ch.qos.logback",
-            name = "logback-classic",
-            version = "1.2.3")
-    testCompile(group = "junit",
-                name = "junit",
-                version = "4.12")
+    compile(
+        group = "com.discord4j",
+        name = "Discord4J",
+        version = "2.10.1"
+    )
+    compile(
+        group = "com.sedmelluq",
+        name = "lavaplayer",
+        version = "1.3.7"
+    )
+    compile(
+        group = "ch.qos.logback",
+        name = "logback-classic",
+        version = "1.2.3"
+    )
+    compile(
+        group = "io.github.microutils",
+        name = "kotlin-logging",
+        version = "1.5.4"
+    )
+    compile(
+        group = "ch.qos.logback",
+        name = "logback-classic",
+        version = "1.2.3"
+    )
+    testCompile(
+        group = "junit",
+        name = "junit",
+        version = "4.12"
+    )
 }
 
 configure<JavaPluginConvention> {
@@ -40,4 +53,21 @@ configure<JavaPluginConvention> {
 }
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+val fatJar = task("fatJar", type = Jar::class) {
+    baseName = "${project.name}-fat"
+    manifest {
+        attributes["Implementation-Title"] = "xyz.pheonic.MusicBot"
+        attributes["Implementation-Version"] = version
+        attributes["Main-Class"] = "xyz.pheonic.musicbot.MainKt"
+    }
+    from(configurations.runtime.map { if (it.isDirectory) it else zipTree(it) })
+    with(tasks["jar"] as CopySpec)
+}
+
+tasks {
+    "build" {
+        dependsOn(fatJar)
+    }
 }
